4bc24233eeadc8e81b2aa87b865feb8ca9828f8d
jinwoo park <swat018@gmail.com>
2022-08-12 17:16:58 +0900

    feat : 1. spring initializr를 사용하여, 프로젝트 생성

---
+++ src/main/java/com/mangosystem/knutwebgis/KnutWebgisApplication.java
@@ -0,0 +1,13 @@
+package com.mangosystem.knutwebgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnutWebgisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnutWebgisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mangosystem/knutwebgis/KnutWebgisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangosystem.knutwebgis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnutWebgisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
